3f07a7198681b2396a5414f8bfd69a7f283e6d05
lc131812 <lc131812@alibaba-inc.com>
2018-06-19 17:46:40 +0800

    zshrc update

---
+++ auto.sh
@@ -0,0 +1,11 @@
+#########################################################################
+# File Name: auto.sh
+# Author: lee
+# mail: lc131812@alibaba-inc.com
+#Created Time:2018年06月19日 星期二 17时42分37秒
+#########################################################################
+#!/bin/bash
+
+mkdir -p ~/.config/terminator
+cp config ~/.config/terminator/
+cp .zshrc ~/.zshrc 
